9bb063d8fa7534c4667794cfe2a2d2b65c575601
Minh Triet <bindsu63@gmail.com>
2022-04-26 00:44:55 +0700

    change api url ^^!

--- src/axios/estate.js
+++ src/axios/estate.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "https://estate-server-api.herokuapp.com/api",
-  baseURL: "http://76b7-2402-800-63b5-c9a8-a0f3-c5fc-728a-a29a.ngrok.io/api",
+  baseURL: "https://estate-server-api.herokuapp.com/api",
 });
